4674e68bae28e6909c33da6754cee1e9014f018d
manuel <monolo150@gmail.cm>
2011-09-17 20:08:30 +0200

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link herf="css/index.css"></link>
+        <script src="js/procesing.js"></script>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        <div id="game">
+            <canvas id="canvas1"data-processing-sources="js/copter.pjs"></canvas>
+        </div>
+    </body>
+</html>
